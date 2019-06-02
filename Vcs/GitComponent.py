fe0facd630c88af1c3b6e9077ba1b1ee14d69f8a
Gisli <gisli@busmoneygames.com>
2019-06-02 20:08:34 +0000

    Hook up the commit ID if its available

--- Vcs/GitComponent.py
+++ Vcs/GitComponent.py
@@ -1,16 +1,31 @@
 import pathlib
 import os
 import json
+import git
+
+
+def get_current_head(root_path):
+    repo = git.Repo(root_path)
+    sha = repo.head.object.hexsha
+    short_sha = repo.git.rev_parse(sha, short=7)
+
+    return short_sha
+
 
 def write_simple_info(run_config):
+
     config_folder_abs_path = run_config["environment"]["sentinel_config_root_path"]
+    project_root_path = run_config["environment"]["version_control_root"]
     path = pathlib.Path(config_folder_abs_path)
 
     version_control_root_path = path.joinpath("_version_control")
     if not version_control_root_path.exists():
         os.makedirs(version_control_root_path)
 
-    simple_info = {"commit_id": "adfadfadsfa"}
+    get_current_head(project_root_path)
+    simple_info = {
+        "commit_id": get_current_head(project_root_path)
+    }
 
     version_control_file = version_control_root_path.joinpath("vcs_info.json")
     f = open(version_control_file, "w")
