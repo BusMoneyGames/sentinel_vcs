d499e682e1eb711d192126978b7f9f228a43f170
Gisli <gisli@busmoneygames.com>
2019-06-02 19:57:02 +0000

    Generates the vcs file as temporary config

--- SentinelVCSComponent.py
+++ SentinelVCSComponent.py
@@ -24,41 +24,15 @@ def _read_config(path):
         quit(1)
 
 
-def _load_environment_config(overwrite_path=""):
-    """Finds the config file that contains the environment information"""
-    # Figure out where the script is run from
-    current_run_directory = pathlib.Path(os.getcwd())
-    L.debug("Current Directory: %s ", current_run_directory)
-
-    if overwrite_path:
-        L.debug("environment config read from none default location")
-        L.debug("relative environment path is: %s", overwrite_path)
-    else:
-        overwrite_path = ".."
-        L.debug("Using the default relative path that resolves to:  %s", overwrite_path)
-
-    config_file_name = "_sentinel_root.json"
-    config_file_path = current_run_directory.joinpath(overwrite_path, config_file_name).resolve()
-    L.debug("Searching for environment file at: %s", config_file_path)
-    L.debug("environment file exists: %s ", config_file_path.exists())
-
-    if config_file_path.exists():
-        return config_file_path
-    else:
-        L.error("Unable to find config environment file")
-        L.error("Expected Path: %s", config_file_path)
-        quit(1)
-
-
 @click.group()
-@click.option('--path', default="", help="path to the config overwrite folder")
+@click.option('--project_root', default="", help="path to the config overwrite folder")
 @click.option('--debug', default=False, help="Turns on debug messages")
 @click.pass_context
-def cli(ctx, path, debug):
+def cli(ctx, project_root, debug):
     """Sentinel Unreal Component handles running commands interacting with unreal engine"""
 
     ctx.ensure_object(dict)
-    ctx.obj['CONFIG_OVERWRITE'] = path
+    ctx.obj['PROJECT_ROOT'] = project_root
 
     if debug:
         L.setLevel(logging.DEBUG)
@@ -76,11 +50,8 @@ def cli(ctx, path, debug):
 def refresh(ctx, output):
     """Generates a config file """
 
-    config_path = ctx.obj['CONFIG_OVERWRITE']
-
-    sentinel_environment_config = _load_environment_config(config_path)
-
-    environment_config = _read_config(sentinel_environment_config)
+    config_path = pathlib.Path(ctx.obj['PROJECT_ROOT']).joinpath("_generated_sentinel_config.json")
+    environment_config = _read_config(config_path)
     GitComponent.write_simple_info(environment_config)
 
 
--- Vcs/GitComponent.py
+++ Vcs/GitComponent.py
@@ -1,5 +1,18 @@
 import pathlib
+import os
+import json
 
-def write_simple_info(environment_config):
-    print(type(environment_config))
-    print(environment_config["sentinel_config_root_path"])
+def write_simple_info(run_config):
+    config_folder_abs_path = run_config["environment"]["sentinel_config_root_path"]
+    path = pathlib.Path(config_folder_abs_path)
+
+    version_control_root_path = path.joinpath("_version_control")
+    if not version_control_root_path.exists():
+        os.makedirs(version_control_root_path)
+
+    simple_info = {"commit_id": "adfadfadsfa"}
+
+    version_control_file = version_control_root_path.joinpath("vcs_info.json")
+    f = open(version_control_file, "w")
+    f.write(json.dumps(simple_info, indent=4))
+    f.close()
